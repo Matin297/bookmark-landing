9c9eae2a7c938d6ac34975c2b1644610c1478671
asma mir <asmamir297@gmail.com>
2021-06-04 17:08:13 +0430

    Responsive improvements in layout

--- components/layout/layout.js
+++ components/layout/layout.js
@@ -6,8 +6,10 @@ import Footer from '../footer/footer';
 import NvgMenu from '../nvg-menu/nvg-menu';
 
 const StyledMain = styled.main`
-    margin-top: 4.0625rem;
-    position: relative
+    margin-top: 6.0625rem;
+    position: relative;
+    display: flex;
+    flex-direction: column;
 `;
 
 function Layout({ children }) {
@@ -15,14 +17,14 @@ function Layout({ children }) {
     const [isMenuOpen, setIsMenuOpen] = useState(false);
 
     return (
-        <>
+        <div>
             <Header setIsMenuOpen={setIsMenuOpen} isMenuOpen={isMenuOpen} />
             <StyledMain>
                 <NvgMenu isMenuOpen={isMenuOpen} />
                 {children}
             </StyledMain>
             <Footer />
-        </>
+        </div>
     );
 };
 
